028c50077f7f489bd7008c2e2307e7c100e04bb5
Jhon Villalobos <villajohn@yahoo.com>
2024-02-22 08:28:31 -0500

    first comit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Panamerica JKA Colombia 2023</title>
+<link rel="apple-touch-icon" sizes="57x57" href="landing/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="landing/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="landing/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="landing/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="landing/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="landing/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="landing/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="landing/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="landing/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="landing/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="landing/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="landing/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="landing/favicon/favicon-16x16.png">
+<link rel="manifest" href="landing/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="landing/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<link href="landing/tools/style.css" rel="stylesheet" type="text/css" media="screen" />
+
+<script src="https://kit.fontawesome.com/5d66303592.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="landing/tools/jquery.min.js"></script> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script> 
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto+Slab:700' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+
+
+</head>
+<body>
+<div class="wrapper">
+  <div class="left-section">
+    <img src="landing/images/logo.png" alt="nova auto rental miami" />
+  </div>
+  <div class="right-section">
+    <h1 id="logo" align="center">Danto's App</h1>
+    <div class="seprator"></div>
+    <div class="main-content">
+      <h2>Let's Grow Together</h2><br>
+      <h2></h2><br>
+      <h2></h2>
+      <p>&nbsp;</p>
+    </div>
+    <ul class="social">
+      <!-- <li id="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li> -->
+      <!-- <li id="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li> -->
+      <!-- <li id="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li> -->
+    </ul>
+    <ul class="info">
+      <!-- <li><i class=""></i><b>ORLANDO</b></li>
+      <li><i class="fa fa-map-marker"></i>5323 Millenia Lakes Blvd Suite 300. Orlando, FL. 32839-3395</li>
+      <li><i class="fa fa-phone-square"></i>786-556.6165</li>
+      <li><i class="fa fa-envelope"></i><a href="#">info@novaautorental.com</a></li> -->
+      <li><i class=""></i>&nbsp;</li>
+      <!-- <li><i class=""></i><b>MIAMI</b></li> -->
+      <!-- <li><i class="fa fa-map-marker"></i>2470 NW 33rd. Avenue. Miami, FL. 33142</li> -->
+      <!-- <li><i class="fa fa-phone-square"></i>786-556.6165</li> -->
+      <li><i class="fa fa-envelope"></i><a href="mailto:secretario@jkacolombia.org">hello@dantosapp.com</a></li>
+      <li id="facebook"><a href="https://www.dantosapp.com"><i class="fa fa-globe"></i>www.dantosapp.com</a></li>
+      <li id="instagram"><a href="https://www.instagram.com/dantosapp"><i class="fa-brands fa-instagram"></i>@dantosapp</a></li>
+    </ul>
+
+
+    <div class="social-links">
+      <ul class="social2">
+        <li id="facebook"><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+        <li id="instagram"><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+        <li id="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+    
+    
+  </div>
+  <div class="watch-section"><img src="landing/images/logo.png" /></div>
+</div>
+
+
+</body>
+</html>
+
---
+++ landing/favicon/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="../favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="../favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+	<link rel="manifest" href="../favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="../favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	  
+	<title>
+		Danto's App - Feel it, we create it - We create something for everyone 
+	</title>
+
+	<!--     Fonts and icons     -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<!-- Nucleo Icons -->
+	<link href="../css/nucleo-icons.css" rel="stylesheet" />
+	<link href="../css/nucleo-svg.css" rel="stylesheet" />
+	<!-- Font Awesome Icons -->
+	<link href="../css/font-awesome.css" rel="stylesheet" />
+	<link href="../css/nucleo-svg.css" rel="stylesheet" />
+	<!-- CSS Files -->
+	<link href="../css/argon-design-system.css?v=1.0.2" rel="stylesheet" />
+</head>
+
+<body class="error-page">
+
+  	<div class="wrapper">
+    	<div class="page-header error-page">
+      		<div class="page-header-image" style="background-image: url('../img/ill/404.svg');"></div>
+      		<div class="container">
+				<div class="row">
+					<div class="col-md-4">&nbsp;</div>
+					<div class="col-md-4">
+						<img class="img-fluid rounded-circle" src="../img/brand/logo.png">
+					</div>	
+					<div class="col-md-4">&nbsp;</div>
+				</div>
+        		<div class="row">
+          			<div class="col-md-12 text-center">
+            			<h5 class="display-3"><br>Hello Travel! Looks like you got lost.</h5>
+            			<p class="lead"><a href="../../Home" class="btn btn-primary">Return to our spaceship</a></p>
+          			</div>
+        		</div>
+      		</div>
+    	</div>
+  </div>
+</body>
+
+</html>
